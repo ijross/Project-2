565a984ee77bab95d43e6a0d1c9e4730ae199a8c
jcheun <jcheun@ucsc.edu>
2014-04-26 20:45:13 -0700

    Update schedproc.h

--- schedproc.h
+++ schedproc.h
@@ -23,7 +23,7 @@ EXTERN struct schedproc {
 	unsigned max_priority;	/* this process' highest allowed priority */
 	unsigned priority;		/* the process' current priority */
 	unsigned time_slice;		/* this process's time slice */
-	undigned ticket_num;
+	unsigned ticket_num;
 } schedproc[NR_PROCS];
 
 /* Flag values */
